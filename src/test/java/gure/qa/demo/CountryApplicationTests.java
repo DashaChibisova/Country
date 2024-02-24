057061e2ae41f74ac9583d01064c12c0288affb6
Darya Chibisova <darya.chibisova@raiffeisen.ru>
2024-02-24 19:31:36 +0300

    Add to controller post/patch
    (cherry picked from commit 159c6587081849412dba4a878396487a2c6689b9)

---
+++ src/test/java/gure/qa/demo/CountryApplicationTests.java
@@ -0,0 +1,13 @@
+package gure.qa.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
